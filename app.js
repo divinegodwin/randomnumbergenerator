484bc672ecc07c6bbbb60c32a1c7891a5d5c4be5
Divine Godwin <79890596+divinegodwin@users.noreply.github.com>
2021-07-09 18:45:37 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function Generate(){
+var randomNumber= (Math.floor(Math.random()*100)+1);
+document.getElementById('number-generated').innerHTML= randomNumber;
+}
+
+function show(){
+    alert('07049974066')
+}
+
+
